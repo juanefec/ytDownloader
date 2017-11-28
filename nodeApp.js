27418bfbf19c32d8bade80ee920b380e903707f7
Juan Clot <juanc_lu@lagash.com>
2017-11-28 09:33:14 -0300

    update

--- nodeApp.js
+++ nodeApp.js
@@ -1,22 +1,22 @@
-var fs = require('fs');
-var yt = require('ytdl-core');
+const fs = require('fs');
+const yt = require('ytdl-core');
 if (process.argv.length < 3) {
   console.log('Usage: node ' + process.argv[1] + ' FILENAME');
   process.exit(1);
 }
 let links = [];
-var filename = process.argv[2];
-fs.readFile(filename, 'utf8', function (err, data) {
+let filename = process.argv[2];
+fs.readFile(filename, 'utf8', (err, data) => {
   if (err) throw err;
   console.log( filename + "  Loaded.");  
-  var exp = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/gm;
-  var regex = new RegExp(exp);
+  let exp = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/gm;
+  let regex = new RegExp(exp);
   links = data.match(regex);
-  links.forEach( link => {    
-    yt.getInfo( yt.getURLVideoID(link) , function (err, info) {
-      if (err) throw err;
-      console.log('title: ' + info.title);
-      console.log("Descargando...");
+  links.forEach( link => { 
+    console.log("link: " + link);
+    yt.getInfo( yt.getURLVideoID(link) , (err, info) => {
+      if (err) throw err;      
+      console.log("Downloading... :" + info.title);      
       yt( link, { filter: "audioonly" }).pipe(fs.createWriteStream(`music/${info.title}.mp3`));
     });
   });
