0ffb708ed40d6238bf6316bc42e4e5abae1804c2
johncGit <clot.juan@gmail.com>
2017-12-04 07:38:04 -0300

    update v1.1

--- ytDwload.js
+++ ytDwload.js
@@ -1,14 +1,11 @@
 const fs = require('fs');
-
 const yt = require('ytdl-core');
-
-
-
 
 if (process.argv.length < 3) {
   console.log('Usage: node ' + process.argv[1] + ' FILENAME');
   process.exit(1);
 }
+
 let links = [];
 let filename = process.argv[2];
 fs.readFile(filename, 'utf8', (err, data) => {
@@ -17,15 +14,13 @@ fs.readFile(filename, 'utf8', (err, data) => {
   let exp = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/gm;
   let regex = new RegExp(exp);
   links = data.match(regex);
-  links.forEach(link => {
-    
+  links.forEach(link => {    
       console.log("Link: " + link);
       yt.getInfo(yt.getURLVideoID(link), (err, info) => {
         if (err) throw err;
         console.log("Downloading... :" + info.title);
-        yt(link, { filter: "audioonly" }).pipe(fs.createWriteStream(`music/${info.title}.mp3`));
-        
-      });
-    
+        yt(link, { filter: (format) => format.container === "m4a"})
+            .pipe(fs.createWriteStream(`music/${info.title}.mp3`));        
+      });    
   });
 });
---
+++ ytInfo.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const fs   = require('fs');
+const ytdl = require('ytdl-core');
+
+if (process.argv.length < 3) {
+    console.log('Usage: node ' + process.argv[1] + ' FILENAME');
+    process.exit(1);
+  }
+const id  = process.argv[2];
+
+const filepath = path.resolve(__dirname, 'info.json');
+
+ytdl.getInfo(id, (err, info) => {
+  if (err) throw err;
+  console.log('title:', info.title);
+  console.log('rating:', info.average_rating);
+  console.log('uploaded by:', info.author.name);
+  const json = JSON.stringify(info, null, 2)
+    .replace(/(ip(?:=|%3D|\/))((?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)|[0-9a-f]{1,4}(?:(?::|%3A)[0-9a-f]{1,4}){7})/ig, '$10.0.0.0');
+  fs.writeFile(filepath, json, err => {
+    if (err) throw err;
+  });
+});
